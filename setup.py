7f3eafd99ef4ed033e8e40f9f98baa69707547ca
Eric Neuman <soloptimus@gmail.com>
2015-07-20 14:32:27 -0400

    one more pypi adjustment

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 setup(
     name = 'django-shannon',
     packages = ['shannon'],
-    version = '1.0.0',
+    version = '1.0.2',
     description='S3 backed django media object.',
     maintainer='Eric Neuman',
     maintainer_email='eric@soloptimus.net',
     url="https://github.com/neuman/django-shannon",
-    download_url = 'https://github.com/neuman/django-shannon/tarball/v1.0.1',
+    download_url = 'https://github.com/neuman/django-shannon/tarball/v1.0.2',
     keywords=['django', 'media','s3','storage']
 )
