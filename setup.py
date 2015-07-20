cbb108b13d3525ce3e3e99564efc86aa8deb08ca
Eric Neuman <soloptimus@gmail.com>
2015-07-20 14:22:58 -0400

    doing stuff for pypi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name = 'django-shannon',
+    packages = ['django-shannon'],
+    version = '1.0.0',
+    description='S3 backed django media object.',
+    maintainer='Eric Neuman',
+    maintainer_email='eric@soloptimus.net',
+    url="https://github.com/neuman/django-shannon",
+    download_url = 'https://github.com/neuman/django-shannon/tarball/v1.0.0',
+    keywords=['django', 'media','s3','storage']
+)
